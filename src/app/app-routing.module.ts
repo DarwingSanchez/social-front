a4032f1c4836aa9bf9468b686504ad18421919a3
DarwingSanchez <darwing.sanchez@imagineapps.co>
2023-04-18 12:01:45 -0500

    Initial configuration and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './public/home/home/home.component';
+import { LoginComponent } from './public/home/login/login.component';
+import { LoggedInGuard } from './public/core/guards/logged-in/logged-in.guard';
+import { LoggedOutGuard } from './public/core/guards/logged-out/logged-out.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent, canActivate: [LoggedInGuard]},
+  /* { path: 'login', component: LoginComponent, canActivate: [LoggedOutGuard]}, */
+
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: "**", redirectTo: "home" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './public/home/sign-in/sign-in.component';
+import { LoginComponent } from './public/home/login/login.component';
+import { HomeComponent } from './public/home/home/home.component';
+import { CreateMessageComponent } from './public/home/profile/create-message/create-message.component';
+import { MyMessagesComponent } from './public/home/profile/my-messages/my-messages.component';
+import { MessageComponent } from './public/home/components/message/message.component';
+import { HeaderComponent } from './public/home/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    LoginComponent,
+    HomeComponent,
+    CreateMessageComponent,
+    MyMessagesComponent,
+    MessageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
